bd92c516ec66968e4fff87df20f8ed5affb10dd7
Nikita Babko <niki-babko@yandex.ru>
2021-04-06 22:02:43 +0700

    Linting.

--- routes/card.js
+++ routes/card.js
@@ -1,53 +1,53 @@
-const { Router } = require("express");
-const Product = require("../models/product");
-const auth = require("../middleware/auth");
+const { Router } = require('express');
+const Product = require('../models/product');
+const auth = require('../middleware/auth');
 const router = Router();
 
 function mapCartItems(cart) {
-    return cart.items.map(c => ({
-        ...c.productId._doc,
-        id: c.productId.id,
-        count: c.count
-    }));
+  return cart.items.map((c) => ({
+    ...c.productId._doc,
+    id: c.productId.id,
+    count: c.count,
+  }));
 }
 
 function computePrice(products) {
-    return products.reduce((total, product) => {
-        return (total += product.price * product.count);
-    }, 0);
+  return products.reduce((total, product) => {
+    return (total += product.price * product.count);
+  }, 0);
 }
 
 // Добавить в корзину
-router.post("/add", auth, async (req, res) => {
-    const product = await Product.findById(req.body.id);
-    await req.user.addToCart(product);
-    res.redirect("/card");
+router.post('/add', auth, async (req, res) => {
+  const product = await Product.findById(req.body.id);
+  await req.user.addToCart(product);
+  res.redirect('/card');
 });
 
 // Удалить из корзины
-router.delete("/remove/:id", auth, async (req, res) => {
-    await req.user.removeFromCart(req.params.id);
-    const user = await req.user.populate("cart.items.productId").execPopulate();
-    const products = mapCartItems(user.cart);
-    const cart = {
-        products,
-        price: computePrice(products)
-    };
-    res.status(200).json(cart);
+router.delete('/remove/:id', auth, async (req, res) => {
+  await req.user.removeFromCart(req.params.id);
+  const user = await req.user.populate('cart.items.productId').execPopulate();
+  const products = mapCartItems(user.cart);
+  const cart = {
+    products,
+    price: computePrice(products),
+  };
+  res.status(200).json(cart);
 });
 
 // Добавляем "auth" для не зареганых юзеров
-router.get("/", auth, async (req, res) => {
-    const user = await req.user.populate("cart.items.productId").execPopulate();
+router.get('/', auth, async (req, res) => {
+  const user = await req.user.populate('cart.items.productId').execPopulate();
 
-    const products = mapCartItems(user.cart);
+  const products = mapCartItems(user.cart);
 
-    res.render("card", {
-        title: "Корзина",
-        isCard: true,
-        products: products,
-        price: computePrice(products)
-    });
+  res.render('card', {
+    title: 'Корзина',
+    isCard: true,
+    products: products,
+    price: computePrice(products),
+  });
 });
 
 module.exports = router;
